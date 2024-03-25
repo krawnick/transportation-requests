eeaa9f3f4fd3c6bf3bfaa8a28253c92b8e074c78
krawnick <kraw.nick@gmail.com>
2024-03-25 14:47:35 +0500

    init project

---
+++ src/components/ControlPanel/index.tsx
@@ -0,0 +1,3 @@
+export const ControlPanel = () => {
+  return <section></section>
+}
---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,5 @@
+import styles from './Main.module.scss'
+
+export const Main = () => {
+  return <main className={styles.wrapper}>Main Page</main>
+}
